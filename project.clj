5f49ef9da6cfe126c509ef3ff2fa79403ad68eeb
Simon Okwori <simon.okwori@gmail.com>
2022-06-30 15:38:41 +0200

    CON-1035 Switch to official http-kit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fullcontact/full.http "1.0.8-SNAPSHOT"
   :description "Async HTTP client and server on top of http-kit and core.async."
-  :url "https://github.com/fullcontact/full.http"
+  :url "https://github.com/contactsplusapp/full.http"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
@@ -8,7 +8,8 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
                  [org.clojure/core.async "0.7.559"]
-                 [fullcontact/http-kit "2.2.1"]
+                 ;[fullcontact/http-kit "2.2.1"]
+                 [http-kit "2.6.0"]
                  [compojure "1.3.4" :exclusions [clj-time]]
                  [javax.servlet/servlet-api "2.5"]
                  [ring-cors "0.1.7"]
@@ -22,7 +23,7 @@
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "--no-sign"]
-                  ["deploy" "clojars"]
+                  ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
